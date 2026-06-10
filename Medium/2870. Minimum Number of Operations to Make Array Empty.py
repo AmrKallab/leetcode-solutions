2779a6d49069ad0643871bcbb0c26ead3d5774be
AmrKallab <amr.qallab556@gmail.com>
2026-06-10 19:32:42 +0300

    .

--- Medium/2870. Minimum Number of Operations to Make Array Empty.py
+++ Medium/2870. Minimum Number of Operations to Make Array Empty.py
@@ -1,14 +1,14 @@
-from collections import defaultdict 
-
-nums = [14,12,14,14,12,14,14,12,12,12,12,14,14,12,14,14,14,12,12]
-freq = defaultdict(int)
-for i in nums :
-    freq[i] += 1
-print(freq)
-ans = 0 
-for val in freq.values() :
-    if val % 3 == 0 :
-        ans += val // 3
-    else :
-        ans += val // 3 + 1
-print(ans)
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        freq = defaultdict(int)
+        for i in nums :
+            freq[i] += 1
+        ans = 0 
+        for val in freq.values() :
+            if val == 1 :
+                return -1 
+            if val % 3 == 0 :
+                ans += val // 3
+            else :
+                ans += val // 3 + 1
+        return ans
